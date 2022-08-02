bc3deb6dce0d049be7e24ae34990464638a3c4a6
yogeshreeturkar111 <110226741+yogeshreeturkar111@users.noreply.github.com>
2022-08-02 22:25:55 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+public class Sample
+{
+    public static void Main()
+    {
+        int i, n;
+        double s = 0.0;
+
+
+            Console.Write("\n\n");
+            Console.Write("Calculate the Harmonic number :\n");
+            Console.Write("___________________");
+        n = Convert.ToInt32(Console.ReadLine());
+            Console.Write("\n\n");
+            for (i = 1; i <=n; i++)
+            {
+            Console.Write("1/{0} + ", i);
+            s += 1 / (float)i;
+            }
+        Console.Write("\nnth harmonic value : \n,n,s");
+    }
+}
